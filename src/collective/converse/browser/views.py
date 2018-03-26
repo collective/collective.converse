f009c0fa7abcd1fe22a248ae480196eef21551f4
JC Brand <jc@opkode.com>
2018-03-26 21:14:08 +0200

    Return user JIDs in search results

--- src/collective/converse/browser/views.py
+++ src/collective/converse/browser/views.py
@@ -19,15 +19,18 @@ log = logging.getLogger(__name__)
 class SearchUsers(BrowserView):
 
     def __call__(self, *args, **kw):
-        searchtext = self.request.form.get('q')
+        domain = plone.api.portal.get_registry_record(
+            'collective.converse.interfaces.IXMPPSettings.xmpp_domain'
+        )
+        searchtext = self.request.form.get('q', '')
         # search terms of less then 3 chars return empty list
         if len(searchtext) < 2:
             return []
         panel = UsersOverviewControlPanel(self.context, self.request)
         return json.dumps([{
             'fullname': u['fullname'],
-            'id': u['id']
-            } for u in panel.doSearch(searchtext)])
+            'jid': '{}@{}'.format(u['id'], domain),
+        } for u in panel.doSearch(searchtext)])
 
 
 class XMPPCredentials(BrowserView):
